bd2cfc97f57c83dfd5dd130f914662737db3a9c3
Lukas Gonzales <lukas@soundhard.com>
2026-02-03 12:33:17 -0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    return config;
-  },
+  webpack: (config) => config,
 
   reactStrictMode: true,
 
