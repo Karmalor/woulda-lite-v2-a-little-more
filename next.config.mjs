aff45ce198ba54afef69e0ff11797c609ac94fee
Lukas Gonzales <lukas@soundhard.com>
2026-02-03 12:25:21 -0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,11 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack: (config) => {
+    return config;
   },
+
+  reactStrictMode: true,
 
   images: {
     remotePatterns: NEXT_PUBLIC_SERVER_URL
